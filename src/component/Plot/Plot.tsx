6453edc02d4d37af2e49840650b20fd4e2416ac0
Antonio Ballesteros <antonioballesteros@gmail.com>
2022-06-05 14:05:14 +0200

    refactor: split useEffect on smaller methods

--- src/component/Plot/Plot.tsx
+++ src/component/Plot/Plot.tsx
@@ -50,7 +50,7 @@ export const getAverageYfromX = (x: number, average?: [Point, Point]): number | 
   return y;
 };
 
-export const isAboveAverage = (d: Point, average?: [Point, Point]): boolean => {
+export const isAboveAverage = (d: PlotValue, average?: [Point, Point]): boolean => {
   const y = getAverageYfromX(d.x, average);
 
   if (y === null) {
@@ -60,7 +60,7 @@ export const isAboveAverage = (d: Point, average?: [Point, Point]): boolean => {
   return d.y > y;
 };
 
-const setColor = (d: Point, literals: [Literal, Literal], average?: [Point, Point]) => {
+const setColor = (d: PlotValue, literals: [Literal, Literal], average?: [Point, Point]) => {
   return isAboveAverage(d, average) ? literals[0].color : literals[1].color;
 };
 
@@ -79,6 +79,143 @@ const showLabel = (
 
 const hideLabel = (tooltip: d3.Selection<SVGTextElement, unknown, null, undefined>): void => {
   tooltip.style('visibility', 'hidden');
+};
+
+const addGridLines = (
+  svg: d3.Selection<SVGGElement, unknown, null, undefined>,
+  xScale: d3.ScaleLinear<number, number, never>,
+  yScale: d3.ScaleLinear<number, number, never>,
+  svgWidth: number,
+  svgHeight: number,
+) => {
+  // Add X grid lines with labels
+  const xAxis = d3.axisBottom(xScale).ticks(5).tickSize(-svgHeight);
+  const xAxisGroup = svg
+    .append('g')
+    .attr('transform', `translate(0, ${svgHeight + MARGIN.labelsHeight})`)
+    .call(xAxis);
+  xAxisGroup.select('.domain').remove();
+  xAxisGroup.selectAll('line').attr('stroke', 'rgba(255, 255, 255, 0.2)');
+  xAxisGroup
+    .selectAll('text')
+    .attr('opacity', 0.5)
+    .attr('color', 'white')
+    .attr('font-size', '0.75rem');
+
+  // Add Y grid lines with labels
+  const yAxis = d3.axisLeft(yScale).ticks(5).tickSize(-svgWidth);
+  const yAxisGroup = svg.append('g').call(yAxis);
+  yAxisGroup.select('.domain').remove();
+  yAxisGroup.selectAll('line').attr('stroke', 'rgba(255, 255, 255, 0.2)');
+  yAxisGroup
+    .selectAll('text')
+    .attr('opacity', 0.5)
+    .attr('color', 'white')
+    .attr('font-size', '0.75rem');
+};
+
+const addInfoLabels = (
+  svg: d3.Selection<SVGGElement, unknown, null, undefined>,
+  literals: [Literal, Literal],
+  average: [Point, Point] | undefined,
+  svgWidth: number,
+) => {
+  // Add info labels
+  svg
+    .append('rect')
+    .attr('x', 0)
+    .attr('y', 0 - MARGIN.top / 2)
+    .attr('width', MARGIN.labelsHeight)
+    .attr('height', MARGIN.labelsHeight)
+    .attr('fill', literals[0].color);
+  svg
+    .append('text')
+    .style('font-size', Math.max(MARGIN.labelsHeight / 2, 14))
+    .attr('fill', 'white')
+    .attr('x', MARGIN.labelsHeight + 10)
+    .attr('y', MARGIN.top / 2 + 4)
+    .text(literals[0].label);
+
+  if (average) {
+    svg
+      .append('rect')
+      .attr('x', svgWidth / 2)
+      .attr('y', 0 - MARGIN.top / 2)
+      .attr('width', MARGIN.labelsHeight)
+      .attr('height', MARGIN.labelsHeight)
+      .attr('fill', literals[1].color);
+    svg
+      .append('text')
+      .style('font-size', Math.max(MARGIN.labelsHeight / 2, 14))
+      .attr('fill', 'white')
+      .attr('x', svgWidth / 2 + MARGIN.labelsHeight + 10)
+      .attr('y', MARGIN.top / 2 + 4)
+      .text(literals[1].label);
+  }
+};
+
+const addAverage = (
+  svg: d3.Selection<SVGGElement, unknown, null, undefined>,
+  average: [Point, Point] | undefined,
+  xScale: d3.ScaleLinear<number, number, never>,
+  yScale: d3.ScaleLinear<number, number, never>,
+  minX: number,
+  maxX: number,
+) => {
+  const averageYfromMinX = getAverageYfromX(minX, average);
+  const averageYfromMaxX = getAverageYfromX(maxX, average);
+
+  // Add average if available
+  if (average) {
+    svg
+      .append('line')
+      .attr('id', 'average')
+      .attr('class', 'average')
+      .attr('x1', xScale(average[0].x))
+      .attr('y1', yScale(averageYfromMinX || 0))
+      .attr('x2', xScale(maxX))
+      .attr('y2', yScale(averageYfromMaxX || 0))
+      .attr('stroke', 'blue');
+  }
+};
+
+const addTooltip = (svg: d3.Selection<SVGGElement, unknown, null, undefined>) => {
+  return svg
+    .append('text')
+    .attr('id', 'tooltip')
+    .attr('class', 'tooltip')
+    .style('font-size', 19)
+    .attr('fill', 'white')
+    .style('visibility', 'hidden');
+};
+
+const addCircles = (
+  svg: d3.Selection<SVGGElement, unknown, null, undefined>,
+  data: PlotValue[],
+  literals: [Literal, Literal],
+  average: [Point, Point] | undefined,
+  xScale: d3.ScaleLinear<number, number, never>,
+  yScale: d3.ScaleLinear<number, number, never>,
+) => {
+  // Add circles with values from data
+  svg
+    .selectAll('.circle')
+    .data(data)
+    .enter()
+    .append('circle')
+    .attr('class', 'circle')
+    .attr('fill', (d: PlotValue) => setColor(d, literals, average))
+    .attr('r', 5)
+    .attr('cx', (d: PlotValue) => xScale(d.x))
+    .attr('cy', (d: PlotValue) => yScale(d.y))
+    .attr('data-label', (d: PlotValue): string => d.label || '')
+    .attr('data-d', (d: PlotValue) => JSON.stringify(d))
+    .on('mouseover', (event: any) => {
+      showLabel(event, tooltip);
+    })
+    .on('mouseout', () => hideLabel(tooltip));
+
+  const tooltip = addTooltip(svg);
 };
 
 const Plot = ({ data = [], average, literals = defaultLiteral }: PlotType) => {
@@ -103,27 +240,26 @@ const Plot = ({ data = [], average, literals = defaultLiteral }: PlotType) => {
           return Math.max(prevX, item.x);
         }, Infinity);
 
-    const averageYfromMinX = getAverageYfromX(minX, average);
-
     const maxX = data.reduce((prevX, item) => {
       return Math.max(prevX, item.x);
     }, -Infinity);
+
     const averageYfromMaxX = getAverageYfromX(maxX, average);
 
     const xScale = d3
       .scaleLinear()
       .domain([
-        Math.min(Math.max(d3.min(data, (d) => d.x) - 50, 0), minX),
-        d3.max(data, (d) => d.x) + 50,
+        Math.min(Math.max(d3.min(data, (d: any) => d.x) - 50, 0), minX),
+        d3.max(data, (d: any) => d.x) + 50,
       ])
       .range([0, svgWidth]);
 
     const yScale = d3
       .scaleLinear()
       .domain([
-        Math.max(d3.min(data, (d) => d.y) - 50, 0),
+        Math.max(d3.min(data, (d: any) => d.y) - 50, 0),
         Math.max(
-          d3.max(data, (d) => d.y),
+          d3.max(data, (d: any) => d.y),
           averageYfromMaxX || 0,
         ) + 50,
       ])
@@ -134,101 +270,10 @@ const Plot = ({ data = [], average, literals = defaultLiteral }: PlotType) => {
     svgEl.selectAll('*').remove(); // Clear svg content before adding new elements
     const svg = svgEl.append('g').attr('transform', `translate(${MARGIN.left},${MARGIN.top})`);
 
-    // Add X grid lines with labels
-    const xAxis = d3.axisBottom(xScale).ticks(5).tickSize(-svgHeight);
-    const xAxisGroup = svg
-      .append('g')
-      .attr('transform', `translate(0, ${svgHeight + MARGIN.labelsHeight})`)
-      .call(xAxis);
-    xAxisGroup.select('.domain').remove();
-    xAxisGroup.selectAll('line').attr('stroke', 'rgba(255, 255, 255, 0.2)');
-    xAxisGroup
-      .selectAll('text')
-      .attr('opacity', 0.5)
-      .attr('color', 'white')
-      .attr('font-size', '0.75rem');
-
-    // Add Y grid lines with labels
-    const yAxis = d3.axisLeft(yScale).ticks(5).tickSize(-svgWidth);
-    const yAxisGroup = svg.append('g').call(yAxis);
-    yAxisGroup.select('.domain').remove();
-    yAxisGroup.selectAll('line').attr('stroke', 'rgba(255, 255, 255, 0.2)');
-    yAxisGroup
-      .selectAll('text')
-      .attr('opacity', 0.5)
-      .attr('color', 'white')
-      .attr('font-size', '0.75rem');
-
-    // Add info labels
-    svg
-      .append('rect')
-      .attr('x', 0)
-      .attr('y', 0 - MARGIN.top / 2)
-      .attr('width', MARGIN.labelsHeight)
-      .attr('height', MARGIN.labelsHeight)
-      .attr('fill', literals[0].color);
-    svg
-      .append('text')
-      .style('font-size', Math.max(MARGIN.labelsHeight / 2, 14))
-      .attr('fill', 'white')
-      .attr('x', MARGIN.labelsHeight + 10)
-      .attr('y', MARGIN.top / 2 + 4)
-      .text(literals[0].label);
-
-    if (average) {
-      svg
-        .append('rect')
-        .attr('x', svgWidth / 2)
-        .attr('y', 0 - MARGIN.top / 2)
-        .attr('width', MARGIN.labelsHeight)
-        .attr('height', MARGIN.labelsHeight)
-        .attr('fill', literals[1].color);
-      svg
-        .append('text')
-        .style('font-size', Math.max(MARGIN.labelsHeight / 2, 14))
-        .attr('fill', 'white')
-        .attr('x', svgWidth / 2 + MARGIN.labelsHeight + 10)
-        .attr('y', MARGIN.top / 2 + 4)
-        .text(literals[1].label);
-    }
-    // Add average if available
-    if (average) {
-      svg
-        .append('line')
-        .attr('id', 'average')
-        .attr('class', 'average')
-        .attr('x1', xScale(average[0].x))
-        .attr('y1', yScale(averageYfromMinX || 0))
-        .attr('x2', xScale(maxX))
-        .attr('y2', yScale(averageYfromMaxX || 0))
-        .attr('stroke', 'blue');
-    }
-
-    // Add circles with values from data
-    svg
-      .selectAll('.circle')
-      .data(data)
-      .enter()
-      .append('circle')
-      .attr('class', 'circle')
-      .attr('fill', (d) => setColor(d, literals, average))
-      .attr('r', 5)
-      .attr('cx', (d) => xScale(d.x))
-      .attr('cy', (d) => yScale(d.y))
-      .attr('data-label', (d): string => d.label || '')
-      .attr('data-d', (d) => JSON.stringify(d))
-      .on('mouseover', (event: any) => {
-        showLabel(event, tooltip);
-      })
-      .on('mouseout', () => hideLabel(tooltip));
-
-    const tooltip = svg
-      .append('text')
-      .attr('id', 'tooltip')
-      .attr('class', 'tooltip')
-      .style('font-size', 19)
-      .attr('fill', 'white')
-      .style('visibility', 'hidden');
+    addGridLines(svg, xScale, yScale, svgWidth, svgHeight);
+    addInfoLabels(svg, literals, average, svgWidth);
+    addAverage(svg, average, xScale, yScale, minX, maxX);
+    addCircles(svg, data, literals, average, xScale, yScale);
   }, [data, svgWidth, svgHeight, literals, average]); // Redraw chart if data changes
 
   return (
